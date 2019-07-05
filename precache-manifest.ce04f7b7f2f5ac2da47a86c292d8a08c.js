f8b7b1fb3d8a69b57cd21a864a6ebabe6c818bc1
Kamilimak123 <krychcik16@gmail.com>
2019-07-05 11:57:10 +0200

    Updates

---
+++ precache-manifest.ce04f7b7f2f5ac2da47a86c292d8a08c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96720f41de334902644505ce23f59e45",
+    "url": "/BioInfoBank-usersList/index.html"
+  },
+  {
+    "revision": "8efe37986b5aa9bef873",
+    "url": "/BioInfoBank-usersList/static/css/main.b826d23e.chunk.css"
+  },
+  {
+    "revision": "bdf502cd61055812f3f4",
+    "url": "/BioInfoBank-usersList/static/js/2.d5b0a073.chunk.js"
+  },
+  {
+    "revision": "8efe37986b5aa9bef873",
+    "url": "/BioInfoBank-usersList/static/js/main.2a3e3491.chunk.js"
+  },
+  {
+    "revision": "d9cf0ebedbbed637acec",
+    "url": "/BioInfoBank-usersList/static/js/runtime~main.c358667b.js"
+  }
+]);
